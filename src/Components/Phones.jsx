41ab7436cde4d0c7f731dd1b4c9d31d51f509495
soubj <sobujahmed124@gmail.com>
2023-05-01 19:58:41 +0600

    react file connected with server and taking data from server

---
+++ src/Components/Phones.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useLoaderData,
+  Link,
+} from "react-router-dom";
+
+const Phones = () => {
+  const phones = useLoaderData();
+  return (
+    <div>
+      <h2>phones here : {phones.length}</h2>
+      {phones.map((phone) => (
+        <li key={phone.id}>
+          <Link to={`/phone/${phone.id}`}>{phone.name}</Link>
+        </li>
+      ))}
+    </div>
+  );
+};
+
+export default Phones;
